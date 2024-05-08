22359cd551e085110ca8de67521a37934c08acda
kumarvvijay059 <kumarvvijay059@gmail.com>
2024-05-08 23:16:45 +0530

    non_overlapping_intervals.java

---
+++ non_overlapping_intervals.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class non_overlapping_intervals {
+    class Solution {
+        public int eraseOverlapIntervals(int[][] intervals) {
+            if (intervals.length == 0) return 0;
+
+            Arrays.sort(intervals, (a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+
+            int minErase = 0;
+            int prevEnd = Integer.MIN_VALUE;
+
+            for (int[] interval : intervals) {
+                if (prevEnd > interval[0]) minErase++;
+                else prevEnd = interval[1];
+            }
+
+            return minErase;
+        }
+    }
+}
