98701f8abbc4ca1add6a93f5301b559fb6578415
kumarvvijay059 <kumarvvijay059@gmail.com>
2024-05-22 18:04:46 +0530

    search_ele_in_2d_array_sorted_row_and_column.java

---
+++ search_ele_in_2d_array_sorted_row_and_column.java
@@ -0,0 +1,24 @@
+class search_ele_in_2d_array_sorted_row_and_column
+{
+    public static int matSearch(int mat[][], int N, int M, int X)
+    {
+        int i = 0;
+        int j = M-1;
+        while(i<N && j>=0)
+        {
+            if(mat[i][j]==X)
+            {
+                return 1;
+            }
+            else if(mat[i][j]>X)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return 0;
+    }
+}
