efc8d3a3615e025a4d6e480cff8b0b1cc58e3859
kumarvvijay059 <kumarvvijay059@gmail.com>
2024-06-25 15:13:39 +0530

    /**

---
+++ Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Binary_Search_Tree_to_Greater_Sum_Tree {
+
+     // Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        int sum = 0;
+        public TreeNode bstToGst(TreeNode root) {
+            if(root != null)
+            {
+                bstToGst(root.right);
+                sum = sum + root.val;
+                root.val = sum;
+                bstToGst(root.left);
+            }
+            return root;
+        }
+    }
+}
